77e778a9c6c88ec833267ba8b301169fc6e2e084
selvakkannan <jothis@science.regn.net>
2026-05-27 00:22:37 +0530

    v1

--- demo.py
+++ demo.py
@@ -1,10 +1,1 @@
-def is_hashable(obj):
-    try:
-        hash(obj)
-        return True
-    except TypeError:
-        return False
-
-values = [100, "PASS", [1, 2], {1, 2}, (1, 2), frozenset([1, 2])]
-for v in values:
-    print(v, "->", is_hashable(v))
+print("this is my v1")
